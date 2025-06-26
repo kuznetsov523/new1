1977227386a8c6c3be8c3110bc7b120c23368362
kuznetsov523 <114984164+kuznetsov523@users.noreply.github.com>
2025-06-26 16:23:35 +0300

    script.js

--- script.js
+++ script.js
@@ -15,17 +15,8 @@ function calculatePrice() {
 
     if (!width || !height || !material) return alert("Заполните все поля!");
 
-    // Расчет площади одного изделия
-    const areaOneItem = (width * height / 1_000_000); // Перевод мм² в м²
-
-    // Стоимость одной единицы материала
-    let pricePerUnit = prices[material];
-
-    // Общая цена всех изделий
-    const totalCost = Math.round(areaOneItem * pricePerUnit * quantity * 100) / 100;
-
     // Формирование результата
-    const resultText = ` ${orderNumber} ${material} ${width}x${height} мм\nКоличество: ${quantity}\nОбщая площадь: ${Math.round(areaOneItem*quantity*100)/100} м²\nИтого: ₽${totalCost}`;
+    const resultText = `${orderNumber} ${material} ${width}x${height} мм\nКоличество: ${quantity}\nИтого: ₽${totalCost}`;
 
     document.getElementById('resultArea').value = resultText;
 }
